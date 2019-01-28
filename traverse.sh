ea7dcc7af86523f397af4ea3e55535bb5ce448f3
Shai Nagar <shain@wix.com>
2019-01-28 10:37:09 +0200

    restored txt file scanning

--- traverse.sh
+++ traverse.sh
@@ -12,15 +12,11 @@ for file in "$1"/*
 do
     if [ ! -d "${file}" ] ; then
         echo -e "${INFO} ${file} is a file"
-    elif [ ${file: -4} == ".test" ]; then
-    #[ ${file: -4} == ".txt" ] ||
-#        echo "@@@@@"
-#        echo ${ERROR} $(docker exec bazel-samba cat ${file})
+    elif [ ${file: -4} == ".txt" ]; then
         echo -e "${ERROR} ${file} is a directory"
     elif [ ${file: -4} == ".jar" ]; then
         echo -e "${ERROR} ${file} is a directory"
     else
-#        echo "entering recursion with: ${file}"
         traverse "${file}"
     fi
 done
